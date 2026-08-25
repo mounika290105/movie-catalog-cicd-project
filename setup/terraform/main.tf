d2a1304c3e257790e65dc59c4b27efc709c419b2
mounika290105 <mounikapenmetsa05@gmail.com>
2026-08-25 05:27:44 -0700

    Update GitHub repository location in Terraform config

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -275,7 +275,7 @@ resource "aws_codebuild_project" "codebuild" {
 
   source {
     type            = "GITHUB"
-    location        = "https://github.com/your-org/your-repo"
+    location        = "https://github.com/mounika290105/movie-catalog-cicd-project"
     git_clone_depth = 1
     buildspec       = "buildspec.yml"
   }
